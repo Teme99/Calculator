e06bd4c707625f44f88914d1b28d67b43368c2ef
Teme99 <3t49631@gmail.com>
2020-02-23 11:13:52 -0800

    git commit -m "the structural part"

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-// this js file will handle the functinoality
-<script>
-    {/* this is where the js code goes */}
-    {/* this is a new line */}
-    </script>
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand =''
+        this.operation = undefined
+
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+        this.currentOperand = number
+
+    }
+    chooseOperation(operation){
+
+    }
+    coompute(){
+
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerText
+
+    }
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => () {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
